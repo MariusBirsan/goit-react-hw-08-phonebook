b8b0048cf6a6cb7e4d3cae1ceed4ae260fbc476a
Marius Birsan <marius.birsan90@outlook.com>
2024-03-17 16:18:11 +0200

    Css was updated

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,8 +4,8 @@ import style from '../styles.module.css';
 
 const HomeView = () => (
   <div className={style.view__container}>
-    <h1 className={style.home__title}>Welcome to the phonebook </h1>
-    <p className={style.please}>please</p>
+    <h1 className={style.home__title}>Phonebook </h1>
+    <p className={style.please}>Please</p>
     <div className={style.please__container}>
       <NavLink
         className={({ active }) => (active ? style.active : style.home__linc)}
